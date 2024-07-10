c0b59a4c08298dc0640a539c110e711865faaca4
Vlgowthami <149039589+Vlgowthami@users.noreply.github.com>
2024-07-10 11:57:32 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,10 @@
+public class greater{
+	public static void main(String[] args){
+		int a=10;
+		int b=14;
+		if(a<b)
+			System.out.println("b is greater than a");
+		else
+			System.out.println("a is greater than b");
+	}
+}
