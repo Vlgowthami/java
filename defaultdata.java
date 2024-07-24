53550813f50e8610c1ecf1374c82098b62542e78
Vlgowthami <149039589+Vlgowthami@users.noreply.github.com>
2024-07-24 10:24:58 +0530

    Add files via upload

---
+++ defaultdata.java
@@ -0,0 +1,20 @@
+public class defaultdata{
+		static int var1;
+		static boolean var2;
+		static float var3;
+		static double var4;
+		static String var5;
+		static long var6;
+	public static void main(String[] arg){
+		
+		System.out.println("default values=");
+		System.out.println("var1="+var1);
+		System.out.println("var2="+var2);
+		System.out.println("var3="+var3);
+		System.out.println("var4="+var4);
+		System.out.println("var5="+var5);
+		System.out.println("var6="+var6);
+	
+	}
+}
+	
